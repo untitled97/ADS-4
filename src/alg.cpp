fed985bcf952627b9bc483505bbf277b13137fb1
untitled97 <125086986+untitled97@users.noreply.github.com>
2023-03-25 22:07:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,33 +11,40 @@ int countPairs1(int *arr, int len, int value) {
 }
 int countPairs2(int *arr, int len, int value) {
     int count = 0;
-    for (int i = 0, j = len - 1; i <= j; i++, j = len - 1) {
-        for (; j > i; j--) {
-            if (value == arr[i] + arr[j])
-                count++;
+    int j = len - 1;
+    while (arr[j] >= value)
+        j--;
+    for (int i = 0; i < j; i++) {
+        if (value == arr[i] + arr[j]) {
+            count++;
+            j--;
         }
     }
     return count;
 }
 int countPairs3(int *arr, int len, int value) {
     int count = 0;
-    int i = 0;
-    int left = 0, right = len - 1, middle;
-    while (left <= right) {
-        middle = (left + right) / 2;
-        if (value > *(arr + middle) + *(arr + i) || i == middle) {
-            left = middle + 1;
-        } else if (value < *(arr + middle) + *(arr + i) || i == middle) {
-            right = middle - 1;
-        } else {
-            count++;
-            while (value == (*(arr + middle--) + *(arr + i++)) && middle > i) {
-                count++;
+    for (int i = 0; i < len; i++) {
+        int left = 0, right = len - 1, middle;
+        while (left <= right) {
+            middle = (left + right) / 2;
+            if (value > *(arr + middle) + *(arr + i) || i == middle)
+                left = middle + 1;
+            else if (value < *(arr + middle) + *(arr + i) || i == middle)
+                right = middle - 1;
+            else {
+                while (value == (*(arr + middle) + *(arr + i)) && middle > i) {
+                    count++;
+                    middle--;
+                    i++;
+                }
+                while (value == (*(arr + middle) + *(arr + i)) && middle > i) {
+                    count++;
+                    middle++;
+                    i++;
+                }
+                return count;
             }
-            while (value == (*(arr + middle++) + *(arr + i++)) && middle > i) {
-                count++;
-            }
-            return count;
         }
     }
     return count;
