c233b84cd2ff254b28a5251cab67001c59021172
untitled97 <125086986+untitled97@users.noreply.github.com>
2023-03-26 15:21:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,13 +11,14 @@ int countPairs1(int *arr, int len, int value) {
 }
 int countPairs2(int *arr, int len, int value) {
     int count = 0;
-    int j = len - 1;
-    while (arr[j] >= value)
-        j--;
-    for (int i = 0; i < j; i++) {
-        if (value == arr[i] + arr[j]) {
-            count++;
-            j--;
+    int m = len - 1;
+    while (arr[m] >= value)
+        m--;
+    for (int i = 0; i < m; i++) {
+        for (int j = m; j > i; j--) {
+            if (value == arr[i] + arr[j]) {
+                count++;
+            }
         }
     }
     return count;
@@ -28,22 +29,23 @@ int countPairs3(int *arr, int len, int value) {
         int left = 0, right = len - 1, middle;
         while (left <= right) {
             middle = (left + right) / 2;
-            if (value > *(arr + middle) + *(arr + i) || i == middle)
+            if (value > *(arr + middle) + *(arr + i) || i == middle) {
                 left = middle + 1;
-            else if (value < *(arr + middle) + *(arr + i) || i == middle)
+            } else if (value < *(arr + middle) + *(arr + i) || i == middle) {
                 right = middle - 1;
-            else {
-                while (value == (*(arr + middle) + *(arr + i)) && middle > i) {
+            } else {
+                count++;
+                int bord_mid = middle - 1;
+                while (value == (*(arr + bord_mid) + *(arr + i)) && bord_mid > left) {
                     count++;
-                    middle--;
-                    i++;
+                    bord_mid--;
                 }
-                while (value == (*(arr + middle) + *(arr + i)) && middle > i) {
+                int bord_mid = middle + 1;
+                while (value == (*(arr + bord_mid) + *(arr + i)) && bord_mid < right) {
                     count++;
-                    middle++;
-                    i++;
+                    bord_mid++;
                 }
-                return count;
+                break;
             }
         }
     }
