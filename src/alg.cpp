779001b49cfcae670c31e76e9c48276cb3d2dd70
untitled97 <125086986+untitled97@users.noreply.github.com>
2023-03-26 23:42:36 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,4 +1,32 @@
 // Copyright 2021 NNTU-CS
+int cbinsearch(int* arr, int size, int value, int n) {
+    int left = n, right = size - 1, middle, count = 0;
+    while (left <= right) {
+        middle = (left + right) / 2;
+        if (*(arr + middle) > value) {
+            right = middle - 1;
+        } else if (*(arr + middle) < value) {
+            left = middle + 1;
+        } else {
+            count++;
+            int bord_mid = middle - 1;
+            while ((*(arr + bord_mid)) == value
+                   && (bord_mid >= left)) {
+                count++;
+                bord_mid--;
+            }
+            bord_mid = middle + 1;
+            while ((*(arr + bord_mid) == value)
+                   && (bord_mid <= right)) {
+                count++;
+                bord_mid++;
+            }
+            return count;
+        }
+    }
+    return count;
+}
+
 int countPairs1(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len - 1; i++) {
@@ -9,10 +37,11 @@ int countPairs1(int *arr, int len, int value) {
     }
     return count;
 }
+
 int countPairs2(int *arr, int len, int value) {
     int count = 0;
     int m = len - 1;
-    while (arr[m] >= value)
+    while (arr[m] > value)
         m--;
     for (int i = 0; i < m; i++) {
         for (int j = m; j > i; j--) {
@@ -23,33 +52,11 @@ int countPairs2(int *arr, int len, int value) {
     }
     return count;
 }
+
 int countPairs3(int *arr, int len, int value) {
     int count = 0;
-    for (int i = 0; i < len; i++) {
-        int left = 0, right = len - 1, middle;
-        while (left <= right) {
-            middle = (left + right) / 2;
-            if (value > *(arr + middle) + *(arr + i) || i == middle) {
-                left = middle + 1;
-            } else if (value < *(arr + middle) + *(arr + i) || i == middle) {
-                right = middle - 1;
-            } else {
-                count++;
-                int bord_mid = middle - 1;
-                while (value == (*(arr + bord_mid) + *(arr + i))
-                       && bord_mid > left) {
-                    count++;
-                    bord_mid--;
-                }
-                bord_mid = middle + 1;
-                while (value == (*(arr + bord_mid) + *(arr + i))
-                       && bord_mid < right) {
-                    count++;
-                    bord_mid++;
-                }
-                break;
-            }
-        }
+    for (int i = 0; i < len - 1; i++) {
+        count += cbinsearch(arr, len, value, i);
     }
     return count;
 }
